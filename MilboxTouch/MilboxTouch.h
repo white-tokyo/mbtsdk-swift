9a48c0ef267c10d901ececffff5f8888d15b526f
moajo <mimirosiasd@gmail.com>
2016-05-30 14:21:53 +0900

    initial commit.

---
+++ MilboxTouch/MilboxTouch.h
@@ -0,0 +1,19 @@
+//
+//  MilboxTouch.h
+//  MilboxTouch
+//
+//  Created by TakumiOhtani on 2016/05/30.
+//  Copyright © 2016年 white. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MilboxTouch.
+FOUNDATION_EXPORT double MilboxTouchVersionNumber;
+
+//! Project version string for MilboxTouch.
+FOUNDATION_EXPORT const unsigned char MilboxTouchVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MilboxTouch/PublicHeader.h>
+
+
---
+++ MilboxTouch/Test.swift
@@ -0,0 +1,14 @@
+//
+//  Test.swift
+//  MilboxTouch
+//
+//  Created by TakumiOhtani on 2016/05/30.
+//  Copyright © 2016年 white. All rights reserved.
+//
+
+import Foundation
+
+
+class Test {
+    
+}
